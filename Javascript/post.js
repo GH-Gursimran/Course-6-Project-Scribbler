fde883411dcb76aa3c8757834502926b4e4603f3
GH-Gursirman <sohanpalgursimransingh@gmail.com>
2021-03-28 15:25:29 +0530

    +edit/save func +link fix

--- Javascript/post.js
+++ Javascript/post.js
@@ -18,3 +18,20 @@ function postLiked(){
         }
     }
 
+
+    var edit = "Edit <i class=\"fa fa-pencil-square-o\" aria-hidden=\"true\"></i>"
+    var save = "Save <i class=\"fa fa-floppy-o\" aria-hidden=\"true\"></i>"
+function editsavetoggle(num){
+    if(num === 0){
+        document.getElementById("title-post").contentEditable=true;
+        document.getElementById("paraText").contentEditable=true;
+        document.getElementById("editsavebtn").setAttribute("onclick","editsavetoggle(1)");
+        document.getElementById("editsavebtn").innerHTML = save;
+    }
+    if(num === 1){
+        document.getElementById("title-post").contentEditable=false;
+        document.getElementById("paraText").contentEditable=false;
+        document.getElementById("editsavebtn").setAttribute("onclick","editsavetoggle(0)");
+        document.getElementById("editsavebtn").innerHTML = edit;
+    }
+}
