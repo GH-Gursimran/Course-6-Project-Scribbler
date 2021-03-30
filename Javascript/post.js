17996740fdc575e07fe0ec4ce357f2e5a3667fd9
GH-Gursirman <sohanpalgursimransingh@gmail.com>
2021-03-30 17:22:56 +0530

    Added Comments and Finalization

--- Javascript/post.js
+++ Javascript/post.js
@@ -1,3 +1,11 @@
+/*
+    For adding comments
+This function takes the value from textarea with id that was
+recieved from function call then uses innerhtml to update the
+comments in div with id 'addEventNames', adds comment above the
+last comment
+
+*/
 function addComments(id){
     comment = "<i class=\"fa fa-angle-right\" aria-hidden=\"true\"></i>"
     if(id.value!==""){
@@ -7,8 +15,18 @@ function addComments(id){
     a.value=a.defaultValue;}
 }
 var numberOfClicks=0;
+
+/* 
+    For adding like to post 
+This function takes the number of clicks as variable to set number
+of likes on post, for one single like it says "1 person likes this"
+when button is pressed more than once it will say "X people have liked this"
+
+*/
+
 function postLiked(){
-    document.getElementById("likeBlog").innerHTML = "Liked!";
+    var like = "<i class=\"fa fa-thumbs-up\" aria-hidden=\"true\"></i>"
+    document.getElementById("likeBlog").innerHTML = like +" "+ "Liked!";
     numberOfClicks += 1;
     if(numberOfClicks!=0){
         if (numberOfClicks==1){
@@ -21,8 +39,14 @@ function postLiked(){
     }
 
 
-    var edit = "Edit <i class=\"fa fa-pencil-square-o\" aria-hidden=\"true\"></i>"
-    var save = "Save <i class=\"fa fa-floppy-o\" aria-hidden=\"true\"></i>"
+/*
+    Toggle Between Edit/Save Post
+This function switches the button between edit and save mode by
+taking the number from function and changing attribute content editable
+according to that.
+*/
+var edit = "Edit <i class=\"fa fa-pencil-square-o\" aria-hidden=\"true\"></i>"
+var save = "Save <i class=\"fa fa-floppy-o\" aria-hidden=\"true\"></i>"
 function editsavetoggle(num){
     if(num === 0){
         document.getElementById("title-post").contentEditable=true;
